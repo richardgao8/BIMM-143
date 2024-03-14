409c5e7572fc74e233e6a0c93bb1f67a2186c8a8
Richard Gao <r4gao@ucsd.edu>
2024-03-14 10:50:56 -0700

    df

---
+++ class04/class4.R
@@ -0,0 +1,8 @@
+# My first R script
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l")
+plot(x, sin(x), typ="l", col="blue", lwd=3)
+plot(x, sin(x), typ="l", col="blue", lwd=3, xlab="Hello", ylab="meep")
+plot(x, sin(x), typ="l", col="violet", lwd=3, xlab="Hello", ylab="meep")
